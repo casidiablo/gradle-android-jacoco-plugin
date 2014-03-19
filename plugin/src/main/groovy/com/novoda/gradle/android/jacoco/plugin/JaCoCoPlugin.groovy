b0c4dbc01fd678e6cb9eefac14228729234fc6de
Carl-Gustaf Harroch <carl@novoda.com>
2014-03-19 22:05:59 +0000

    better logic

--- plugin/src/main/groovy/com/novoda/gradle/android/jacoco/plugin/JaCoCoPlugin.groovy
+++ plugin/src/main/groovy/com/novoda/gradle/android/jacoco/plugin/JaCoCoPlugin.groovy
@@ -1,19 +1,18 @@
 package com.novoda.gradle.android.jacoco.plugin
-
 import com.android.build.gradle.AppPlugin
-import com.android.build.gradle.BasePlugin
 import com.android.build.gradle.LibraryPlugin
 import com.android.build.gradle.tasks.Dex
 import com.novoda.gradle.android.jacoco.plugin.tasks.InstrumentTask
-import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.ProjectConfigurationException
+import org.gradle.api.artifacts.Configuration
 import org.gradle.api.tasks.Copy
 import org.gradle.api.tasks.Exec
 import org.gradle.api.tasks.compile.JavaCompile
 
 class JaCoCoPluginExtension {
-    def String version = '0.6.6-SNAPSHOT'
+    def String version = '0.7.1-SNAPSHOT'
 }
 
 class JaCoCoPlugin implements Plugin<Project> {
@@ -22,10 +21,11 @@ class JaCoCoPlugin implements Plugin<Project> {
         def hasApp = project.plugins.hasPlugin(AppPlugin)
         def hasLib = project.plugins.hasPlugin(LibraryPlugin)
         if (!hasApp && !hasLib) {
-            throw new IllegalStateException("'android' or 'android-library' plugin required.")
+            throw new ProjectConfigurationException("'android' or 'android-library' plugin required.")
         }
 
-        project.extensions.create("jacoco", JaCoCoPluginExtension)
+        def extension = project.android.extensions.create("jacoco", JaCoCoPluginExtension)
+
         final def variants
         if (hasApp) {
             variants = project.android.applicationVariants
@@ -33,15 +33,25 @@ class JaCoCoPlugin implements Plugin<Project> {
             variants = project.android.libraryVariants
         }
 
-        project.configurations.create("jacoco")
+        println "-----------------------------> " + project.android.jacoco.version
+
+        Configuration configuration = project.configurations.create("jacoco")
         project.dependencies {
-            jacoco 'org.jacoco:org.jacoco.ant:' + project.jacoco.version
-            jacoco 'org.jacoco:org.jacoco.agent:' + project.jacoco.version
+            jacoco 'org.jacoco:org.jacoco.ant:' + project.android.jacoco.version
+            jacoco 'org.jacoco:org.jacoco.agent:' + project.android.jacoco.version
         }
+        configuration.dependencies.whenObjectAdded {
+            println it.version
+            if (it.version != '0.7.1-SNAPSHOT' ) {println "got it!"}
+           // configuration.dependencies.remove(it)
+        }
+
 
         variants.all { variant ->
             if (variant.getTestVariant()) {
+
                 JavaCompile javaCompile = variant.javaCompile
+                String variantName = variant.getVariantData().getVariantConfiguration().fullName.capitalize()
                 InstrumentTask instrument =
                         project.task("instrument${variant.getVariantData().getVariantConfiguration().fullName.capitalize()}",
                                 type: InstrumentTask) {
@@ -49,7 +59,7 @@ class JaCoCoPlugin implements Plugin<Project> {
                             destinationDir = project.file("${project.buildDir}/instrumented-classes/${variant.getVariantData().getVariantConfiguration().dirName}/")
                         }
 
-                def extractAgent = project.task("extractJacocoAgent", type: Copy) {
+                def extractAgent = project.task("${variantName}extractJacocoAgent", type: Copy) {
                     from(project.configurations.jacoco.collect { project.zipTree(it) }) {
                         include 'jacocoagent.jar'
                     }
@@ -65,7 +75,7 @@ class JaCoCoPlugin implements Plugin<Project> {
                 dex.dependsOn(instrument, extractAgent)
                 instrument.mustRunAfter javaCompile
 
-                def inst = project.task('instrument', type:Exec) {
+                def inst = project.task("${variantName}instrument", type:Exec) {
                     com.android.build.gradle.BasePlugin plugin = project.plugins.findPlugin('android')
                     def pkg = variant.getTestVariant().getVariantData().getPackageName()
                     def runner = variant.getTestVariant().getVariantData().getVariantConfiguration().getInstrumentationRunner()
@@ -73,7 +83,7 @@ class JaCoCoPlugin implements Plugin<Project> {
                     commandLine plugin.extension.adbExe, 'shell', 'am', 'instrument', '-e', 'coverage', 'true', '-w', asstring
                 }
 
-                def pullingCoverageFile = project.task('pullCoverage', type:Exec) {
+                def pullingCoverageFile = project.task("${variantName}pullCoverage", type:Exec) {
                     def f = variant.getVariantData().getPackageName()
                     com.android.build.gradle.BasePlugin plugin = project.plugins.findPlugin('android')
                     commandLine plugin.extension.adbExe, 'pull', "/data/data/$f/files/coverage.ec","$project.buildDir/jacocoreport/coverage.ec"
@@ -83,7 +93,7 @@ class JaCoCoPlugin implements Plugin<Project> {
                 pullingCoverageFile.dependsOn inst
 
                 pullingCoverageFile.doLast {
-                    ant.taskdef(name: 'jacocoReport', classname: 'org.jacoco.ant.ReportTask', classpath: project.configurations.jacoco.asPath)
+                    ant.taskdef(name: "${variantName}jacocoReport", classname: 'org.jacoco.ant.ReportTask', classpath: project.configurations.jacoco.asPath)
                     ant.jacocoReport {
                         executiondata {
                             fileset(dir: "$project.buildDir/jacocoreport/", includes: '**/*.ec')
@@ -103,7 +113,7 @@ class JaCoCoPlugin implements Plugin<Project> {
                     getLogger().lifecycle("Report saved at: $project.buildDir/jacocoreport/index.html")
                 }
 
-                project.task('coverage').dependsOn pullingCoverageFile
+                project.task("${variantName}coverage").dependsOn pullingCoverageFile
             }
         }
     }
